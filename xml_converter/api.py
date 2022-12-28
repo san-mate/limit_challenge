8e033330ad8c719ae23d0c1637259551f80296d4
Martin Santos <m.sanmate@gmail.com>
2022-12-28 17:46:36 +0000

    Adds code to handle file upload with DRF

--- xml_converter/api.py
+++ xml_converter/api.py
@@ -1,14 +1,21 @@
+from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, FileUploadParser
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+from .logic import xml_file_to_json
+from .serializers import UploadFileSerializer
+
 
 class ConverterViewSet(ViewSet):
-    # Note this is not a restful API
-    # We still use DRF to assess how well you know the framework
-    parser_classes = [MultiPartParser]
+    parser_classes = [MultiPartParser, ]
 
     @action(methods=["POST"], detail=False, url_path="convert")
     def convert(self, request, **kwargs):
-        return Response({})
+        serializer = UploadFileSerializer(data=request.data)
+        if serializer.is_valid():
+            uploaded_file = serializer.validated_data['file']
+            json_response = xml_file_to_json(uploaded_file)
+            return Response(json_response)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
